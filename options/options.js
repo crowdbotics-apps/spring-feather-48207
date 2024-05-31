893d700e07585c3fbbfcc75131123ae0ab147edf
Crowdbotics <team@crowdbotics.com>
2024-05-31 18:30:45 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "spring-feather-48207",
+  url: "https://spring-feather-48207.botics.co",
+  api: "https://spring-feather-48207.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
